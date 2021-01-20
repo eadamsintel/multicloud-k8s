34ed80b95f70ee7d16341704ae81d7feed378904
Eric Adams <eric.adams@intelcom>
2021-01-20 17:59:56 +0000

    Fixed installer for containerized path to remove virtlet

--- kud/hosting_providers/containerized/installer.sh
+++ kud/hosting_providers/containerized/installer.sh
@@ -127,7 +127,7 @@ function install_addons {
         tee $cluster_log/setup-kud.log
     # The order of KUD_ADDONS is important: some plugins (sriov, qat)
     # require nfd to be enabled.
-    for addon in ${KUD_ADDONS:-virtlet ovn4nfv nfd sriov qat cmk $plugins_name}; do
+    for addon in ${KUD_ADDONS:-ovn4nfv nfd sriov qat cmk $plugins_name}; do
         echo "Deploying $addon using configure-$addon.yml playbook.."
         ansible-playbook $verbose -i \
             $kud_inventory -e "base_dest=$HOME" $kud_playbooks/configure-${addon}.yml | \
@@ -137,7 +137,7 @@ function install_addons {
     echo "Run the test cases if testing_enabled is set to true."
     if [[ "${testing_enabled}" == "true" ]]; then
         failed_kud_tests=""
-        for addon in ${KUD_ADDONS:-virtlet ovn4nfv nfd sriov qat cmk $plugins_name}; do
+        for addon in ${KUD_ADDONS:-ovn4nfv nfd sriov qat cmk $plugins_name}; do
             pushd $kud_tests
             bash ${addon}.sh || failed_kud_tests="${failed_kud_tests} ${addon}"
             case $addon in
